3dc21890b93f93a4d09e6ef5b420f6ce85349bed
olliepayne <olliepayne@me.com>
2021-02-10 22:17:40 -0700

    implement basic password matching and input handling

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
+
 const SignupForm = () => {
  // import useState, setup inital form data hook
  // handle input changes
  // submit form data through props
+
+ const [formData, setFormData] = useState({
+  username: '',
+  email: '',
+  password: '',
+  passwordCheck: ''
+ })
+
+ const [passwordMatch, setPasswordMatch] = useState(true)
+
+ const handleInputChange = (e) => {
+  const newData = formData
+  newData[e.target.name] = e.target.value
+  setFormData(newData)
+
+  if(e.target.name === 'password' || e.target.name === 'passwordCheck') setPasswordMatch(handlePasswordCheck(newData))
+ }
+
+ const handlePasswordCheck = (newData) => {
+  if(newData.password === newData.passwordCheck) return true
+  return false
+ }
 
  return (
   <form>
@@ -10,9 +34,10 @@ const SignupForm = () => {
    <label>*Email</label>
    <input name="email" type="text" />
    <label>*Password</label>
-   <input name="password" type="password" />
+   <input name="password" type="password" onChange={handleInputChange} />
+   {!passwordMatch && <p>passwords must match</p>}
    <label>*Confirm Password</label>
-   <input name="passwordCheck" type="password" />
+   <input name="passwordCheck" type="password" onChange={handleInputChange} />
   </form>
  )
 }
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,9 +4,6 @@ import Landing from '../Landing/Landing'
 import Signup from '../Signup/Signup'
 
 function App() {
- // import switch and route from react-router-dom
- // import and render signup page, setup switch and signup route
-
  return (
   <div className="App">
    <Switch>
