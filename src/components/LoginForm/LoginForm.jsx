d8a2399ae0d6d7e1098f9b494bb9e763c1c4a048
olliepayne <olliepayne@me.com>
2021-02-14 13:17:07 -0700

    stub frontend login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,47 @@
-const LoginForm = () => {
- return (
-  <form>
+import { useState } from 'react'
+
+const LoginForm = (props) => {
+ // const { handleLogin } = props
+
+ const [formData, setFormData] = useState({
+  email: '',
+  password: ''
+ })
+
+ const handleInputChange = (e) => {
+  const newData = formData
+  newData[e.target.name] = e.target.value
+  setFormData(newData)
+ }
+
+ const isValidForm = () => {
+  for(const key in formData) {
+   console.log(formData[key])
+   if(formData[key] === '') {
+    return false
+   }
+  }
+
+  return true
+ }
+
+ const handleSubmit = (e) => {
+  e.preventDefault()
+
+  if(isValidForm()) {
    
+  }
+ }
+
+ return (
+  <form onSubmit={handleSubmit} >
+   <label>*Email</label>
+   <input name="email" type="text" onChange={handleInputChange} />
+   <label>*Password</label>
+   <input name="password" type="password" onChange={handleInputChange} />
+   <button>Submit</button>
   </form>
  )
 }
 
-export default LoginForm;
+export default LoginForm
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,11 @@
+import LoginForm from '../../components/LoginForm/LoginForm'
+
 const Login = () => {
+ // handle login props
+
  return (
   <div>
-   
+   <LoginForm />
   </div>
  )
 }
