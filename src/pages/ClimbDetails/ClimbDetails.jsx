892930d9066de1704332abcd7e7e27370c27cfef
olliepayne <olliepayne@me.com>
2021-02-15 20:06:54 -0700

    backup

--- src/pages/ClimbDetails/ClimbDetails.jsx
+++ src/pages/ClimbDetails/ClimbDetails.jsx
@@ -1,4 +1,8 @@
 const ClimbDetails = () => {
+ // pass api from app, call backend using useParams hook
+ // return the corresponding climb, and display
+ // if we are the owner of the climb, add the ability to update and delete
+
  return (
   <div>
    
