ddad0b135b061db48f05a4746747a87562d77619
olliepayne <olliepayne@me.com>
2021-02-16 21:22:17 -0700

    fix deployment

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/climbs', climbsRouter)
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
